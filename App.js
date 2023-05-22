e9a6e9ce996530f85c642a02bffd14a5b9f4b130
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-22 16:15:51 +0200

    added Register Screen and func

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
 import UserScreen from "./src/screens/UserScreen";
 import * as React from "react";
 
@@ -25,6 +26,10 @@ export default function App() {
         <Stack.Screen
           name='LoginScreen'
           component={LoginScreen}
+        />
+        <Stack.Screen
+          name='RegisterScreen'
+          component={RegisterScreen}
         />
         <Stack.Screen
           name='TabNavigator'
