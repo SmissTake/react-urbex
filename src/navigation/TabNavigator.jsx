2600e1a773568d9972b3266ec52060a2d1f5351f
Gwion Tangi <lil.smiss7@gmail.com>
2023-07-02 14:05:00 +0200

    style to tabbar

--- src/navigation/TabNavigator.jsx
+++ src/navigation/TabNavigator.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen';
-import CreatePlaceScreen from '../screens/CreatePlaceScreen';
-import UserScreen from '../screens/UserScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import CreatePlaceScreen from "../screens/CreatePlaceScreen";
+import UserScreen from "../screens/UserScreen";
 import { ModalProvider } from "../contexts/ModalContext";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <ModalProvider>
-      <Tab.Navigator>
-        <Tab.Screen name='HomeScreen' component={HomeScreen} />
-        <Tab.Screen name='CreatePlaceScreen' component={CreatePlaceScreen} />
-        <Tab.Screen name='UserScreen' component={UserScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "blue",
+          tabBarTabStyle: {
+            borderRightWidth: 1,
+            borderRightColor: "gray",
+          },
+        }}
+      >
+        <Tab.Screen
+          name='HomeScreen'
+          component={HomeScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='CreatePlaceScreen'
+          component={CreatePlaceScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='plus' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='UserScreen'
+          component={UserScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name='user' color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </ModalProvider>
   );
