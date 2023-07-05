98523e470575b7fce74246d14a1f900fa0f0b550
Gwion Tangi <lil.smiss7@gmail.com>
2023-07-05 09:28:07 +0200

    fixed font use before it loaded warning

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as Font from 'expo-font';
 import Router from "./src/navigation/Router";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
   async function loadFonts() {
     await Font.loadAsync({
       'PolySans': require('./assets/fonts/PolySans/PolySansTrial-Bulky.otf'),
     });
+    setFontLoaded(true);
   }
   
   // Call the loadFonts function when the app starts
   useEffect(() => {
     loadFonts();
   }, []);
+
+  if (!fontLoaded) {
+    loadFonts();
+    return null;
+  }
 
   return (
     <Router />
